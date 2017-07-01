2f0c5238f7e31c9991251c7a4fb8cbaea81bfbf4
ken-hu <xnchnhu@gmail.com>
2017-07-01 18:24:43 +0100

    Add evil-leader, update rtags keybindings

--- init.el
+++ init.el
@@ -7,7 +7,9 @@
 
 (setq custom-file "~/.emacs.d/custom.el")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(setq create-lockfiles nil)
 (setq backup-directory-alist `(("." . "~/.emacs.bk")))
+(setq auto-save-file-name-transforms `((".*" ,"~/.emacs.bk" t)))
 
 (blink-cursor-mode 0)
 (tool-bar-mode -1)
@@ -41,10 +43,16 @@
 
 (eval-when-compile (require 'use-package))
 
+(use-package evil-leader
+  :config
+  (global-evil-leader-mode t)
+)
+
 (use-package evil
   :config
   (evil-mode t)
   (define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
+  (evil-leader/set-leader ",")
 )
 
 (use-package helm
@@ -129,54 +137,54 @@ Repeated invocations toggle between the two most recently open buffers."
 
 (global-set-key (kbd "C-x p") 'switch-to-previous-buffer)
 
-(use-package company-rtags
-)
+(use-package company-rtags)
 
-(add-to-list 'exec-path (expand-file-name "/usr/local/bin"))
 (use-package rtags
   :config
   (setq rtags-completions-enabled t)
+  (setq rtags-use-helm t)
+  (add-to-list 'exec-path (expand-file-name "/usr/local/bin"))
   (eval-after-load 'company
   '(add-to-list
     'company-backends 'company-rtags))
   (setq rtags-autostart-diagnostics t)
   ;;(rtags-enable-standard-keybindings)
   (eval-after-load 'rtags
-    '(progn
-       (mapc (lambda (x)
-               (define-key c-mode-base-map
-                 (kbd (concat "C-x r " (car x))) (cdr x)))
-             '(("s" . rtags-find-symbol-at-point)
-               ("," . rtags-find-references-at-point)
-               ("v" . rtags-find-virtuals-at-point)
-               ("V" . rtags-print-enum-value-at-point)
-               ("/" . rtags-find-all-references-at-point)
-               ("Y" . rtags-cycle-overlays-on-screen)
-               (">" . rtags-find-symbol)
-               ("<" . rtags-find-references)
-               ("-" . rtags-location-stack-back)
-               ("+" . rtags-location-stack-forward)
-               ("D" . rtags-diagnostics)
-               ("G" . rtags-guess-function-at-point)
-               ("p" . rtags-set-current-project)
-               ("P" . rtags-print-dependencies)
-               ("e" . rtags-reparse-file)
-               ("E" . rtags-preprocess-file)
-               ("R" . rtags-rename-symbol)
-               ("M" . rtags-symbol-info)
-               ("S" . rtags-display-summary)
-               ("O" . rtags-goto-offset)
-               (";" . rtags-find-file)
-               ("F" . rtags-fixit)
-               ("X" . rtags-fix-fixit-at-point)
-               ("B" . rtags-show-rtags-buffer)
-               ("I" . rtags-imenu)
-               ("T" . rtags-taglist)))))
-)
+    (dolist (mode '(c-mode c++-mode))
+      (evil-leader/set-key-for-mode mode
+	"s" 'rtags-find-symbol-at-point
+	"r" 'rtags-find-references-at-point
+	"v" 'rtags-find-virtuals-at-point
+	"V" 'rtags-print-enum-value-at-point
+	"ar" 'rtags-find-all-references-at-point
 
-(use-package rtags-helm
-  :config
-  (setq rtags-use-helm t)
+	"fs" 'rtags-find-symbol
+	"fr" 'rtags-find-references
+	"ff" 'rtags-find-file
+
+	"R" 'rtags-rename-symbol
+	"I" 'rtags-symbol-info
+
+	"[" 'rtags-location-stack-back
+	"]" 'rtags-location-stack-forward
+	"Y" 'rtags-cycle-overlays-on-screen
+	"D" 'rtags-diagnostics
+	"G" 'rtags-guess-function-at-point
+	"p" 'rtags-set-current-project
+	"P" 'rtags-print-dependencies
+	"e" 'rtags-reparse-file
+	"E" 'rtags-preprocess-file
+
+	"S" 'rtags-display-summary
+	"O" 'rtags-goto-offset
+	"i" 'rtags-fixit
+	"L" 'rtags-copy-and-print-current-location
+	"X" 'rtags-fix-fixit-at-point
+	"B" 'rtags-show-rtags-buffer
+	"M" 'rtags-imenu
+	"T" 'rtags-taglist
+	"h" 'rtags-print-class-hierarchy
+	"A" 'rtags-print-source-arguments)))
 )
 
 (use-package flycheck-rtags
@@ -191,4 +199,8 @@ Repeated invocations toggle between the two most recently open buffers."
   (add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
 )
 
-(global-set-key (kbd "C-x g") 'magit-status)
+(use-package magit
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status)
+)
+
