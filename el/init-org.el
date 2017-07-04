89320dd1adb24b7076c68db48b819ecdc8e969b8
Ken Hu <xnchnhu@gmail.com>
2017-07-04 19:41:04 +0100

    Update org mode

--- el/init-org.el
+++ el/init-org.el
@@ -3,4 +3,9 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+        (sequence "|" "CANCELED(c)")))
+
 (provide 'init-org)
