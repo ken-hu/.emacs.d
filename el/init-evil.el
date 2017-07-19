d80c05d935f1e0dbbbb7e2eb29f198e4e1836d19
ken-hu <xnchnhu@gmail.com>
2017-07-19 14:52:42 +0100

    Disable mouse mode in evil-insert-mode, update orgmode config

--- el/init-evil.el
+++ el/init-evil.el
@@ -8,6 +8,12 @@
   (evil-mode t)
   (define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
   (evil-leader/set-leader ",")
+  ;; Disable mouse scrollling in insert mode
+  (add-hook 'evil-insert-state-entry-hook (lambda() (mouse-wheel-mode -1)))
+  (add-hook 'evil-insert-state-exit-hook 'mouse-wheel-mode -1)
 )
+
+(with-eval-after-load 'evil
+  (defalias #'forward-evil-word #'forward-evil-symbol))
 
 (provide 'init-evil)
--- el/init-org.el
+++ el/init-org.el
@@ -3,9 +3,33 @@
 (global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
 
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "|" "DONE(d)")
-        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
-        (sequence "|" "CANCELED(c)")))
+;; So org-capture works
+(setq org-default-notes-file "~/Dropbox/org/capture.org")
+(setq org-directory "~/Dropbox/org")
+;; Open a file
+(global-set-key (kbd "C-c o t") (lambda() (interactive) (find-file "~/Dropbox/org/todo.org")))
+(global-set-key (kbd "C-c o n") (lambda() (interactive) (find-file "~/Dropbox/org/notes.org")))
+;; Code block highlight in orgmode
+(setq org-src-fontify-natively t)
+;; #+STARTUP: showeverything
+(setq org-startup-folded nil)
+
+(setq org-capture-templates
+      '(
+        ("t" "TODO tasks" entry (file "todo.org")
+         "* TODO%?\n%u\n" :empty-lines 1)
+
+        ("n" "NOTE" entry (file+headline "notes.org" "Work")
+         "* %?\n%u\n" :empty-lines 1)
+
+        ("c" "CheckBox" checkitem (file+headline "checkbox.org" "Checkbox")
+         "[ ] %?\n%u\n" :empty-lines 1)
+       )
+)
+
+;;(setq org-todo-keywords
+;;      '((sequence "TODO(t)" "|" "DONE(d)")
+;;        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+;;        (sequence "|" "CANCELED(c)")))
 
 (provide 'init-org)
